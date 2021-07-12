3c5cd69a1e109be54e3e2588d72447876edcf815
adam <adammajid051@gmail.com>
2021-07-13 05:22:42 +0700

    First Push

---
+++ app/Database/Migrations/2021-07-12-205505_User.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+use phpDocumentor\Reflection\PseudoTypes\True_;
+
+class User extends Migration
+{
+	public function up()
+	{
+		$this->forge->addField([
+			'id' => [
+				'type' => 'INT',
+				'constraint' => 11,
+				'unsigned' => TRUE,
+				'auto_increment' => TRUE
+			],
+			'username' => [
+				'type' => 'VARCHAR',
+				'constraint' => 11
+			],
+			'password' => [
+				'type' => 'TEXT'
+			],
+			'salt' => [
+				'type' => 'TEXT'
+
+			],
+			'avatar' => [
+				'type' => 'TEXT',
+				'null' => TRUE
+			],
+			'role' => [
+				'type' => 'INT',
+				'constraint' => 1,
+				'default' => 1
+			],
+			'created_by' => [
+				'type' => 'INT',
+				'constraint' => 11,
+			],
+			'created_date' => [
+				'type' => 'DATETIME',
+			],
+			'updated_by' => [
+				'type' => 'INT',
+				'constraint' => 11,
+				'null' => TRUE
+			],
+			'updated_date' => [
+				'type' => 'DATETIME',
+				'null' => TRUE
+			]
+		]);
+
+		$this->forge->addKey('id', TRUE);
+		$this->forge->createTable('user');
+	}
+
+	public function down()
+	{
+		$this->forge->dropTable('user');
+	}
+}
---
+++ app/Views/hello/world.php
@@ -0,0 +1,4 @@
+<?= $this->extend('layout'); ?>
+<?= $this->section('content'); ?>
+    <h1>hello world</h1>
+<?= $this->endSection(); ?>
